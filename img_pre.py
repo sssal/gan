984c78d64c1dca2c899cddf0ec9ee62471f0e699
sssal <43909189+sssal@users.noreply.github.com>
2019-05-22 20:10:00 +0800

    Add files via upload

--- img_pre.py
+++ img_pre.py
@@ -8,6 +8,11 @@ def get_image(img_path,output_height=48,output_width=48):
     image_b = cv2.resize(image,(output_height,output_width))
     return image_b.astype(np.float)
 
+def get_image_rgb(img_path,output_height=48,output_width=48):
+    image = cv2.imread(img_path,flags=1) #得到灰度图
+    image_b = cv2.resize(image,(output_height,output_width))
+    return image_b.astype(np.float)
+	
 # 标准化
 def stand(image):
     return np.array(image)/127.5-1.
@@ -22,17 +27,34 @@ def twod2oned(image,output_height=48,output_width=48):
 def oned2twod(image,output_height=48,output_width=48):
     return np.resize(image,[output_height,output_width])
 
+def thrd2oned(image,output_height=48,output_width=48):
+    return np.resize(image,[output_height*output_width*3])
+
+def oned2thrd(image,output_height=48,output_width=48):
+    return np.resize(image,[output_height,output_width,3])
+
 def pre_processing(img_path,output_height=48,output_width=48):
     img = get_image(img_path)
     img = stand(img)
     img = twod2oned(img)
     return img
 
+def pre_processing_rgb(img_path,output_height=48,output_width=48):
+    img = get_image_rgb(img_path)
+    img = stand(img)
+    img = thrd2onedd2oned(img)
+    return img
+	
 def af_processing(img):
     img = oned2twod(img)
     img = stand_r(img)
     return img
 
+def af_processing_rgb(img):
+    img = thrd2oned(img)
+    img = stand_r(img)
+    return img
+	
 # if __name__ == '__main__':
 #     data_path = r'./data/faces/*.jpg'
 #     data = glob(data_path)
